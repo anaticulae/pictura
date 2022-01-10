325fd813cf714d8914af393b9e2035c58b91efe3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-10 14:25:24 +0100

    test(detect): add method to verify detector behavior/interface

---
+++ tests/test_detector.py
@@ -0,0 +1,32 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import os
+
+import pytest
+import utila
+
+import picture
+import power
+
+
+@pytest.mark.xfail(reason='incomplete impl')
+def test_image_content():
+    sources = os.path.join(
+        power.link(power.MASTER063_PDF),
+        'rawmaker__images_images',
+    )
+    images = utila.file_list(sources, exclude='yaml', absolute=True)
+    first = images[0]
+    loaded = picture.imageload(first)
+    detected = picture.detect(loaded)
+    assert detected.detected == picture.ImageType.LOGO
+    # TODO: ENABLE LATER
+    expected = 'IN DEI NOMINE FELICITER\nRADBOUD UNIVERSITEIT NIJMEGEN'
+    assert detected.text == expected
